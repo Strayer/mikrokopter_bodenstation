2c1055195a70e84bd2dd321b0eda730f4f104b12
Sven Grunewaldt <strayer@olle-orks.org>
2011-11-14 22:20:40 +0100

    Parameter sind nun nach type_id sortiert

--- widgets/parameterswidget.cpp
+++ widgets/parameterswidget.cpp
@@ -15,6 +15,144 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	connect(m_signalMapper, SIGNAL(mapped(QWidget*)), this, SLOT(parameterChanged(QWidget*)));
 	this->m_settings = new QSettings(qApp->applicationDirPath().append("/parameter_profiles.ini"), QSettings::IniFormat);
 
+	QGroupBox *yawBox = new QGroupBox(tr("YAW"));
+	QFormLayout *yawBoxLayout = new QFormLayout();
+	yawBox->setLayout(yawBoxLayout);
+
+	ParameterSpinBox *YAW_KP = new ParameterSpinBox(Parameters::YAW_KP);
+	YAW_KP->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(YAW_KP);
+	yawBoxLayout->addRow(tr("KP:"), YAW_KP);
+	m_signalMapper->setMapping(YAW_KP, YAW_KP);
+	connect(YAW_KP, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *YAW_KI = new ParameterSpinBox(Parameters::YAW_KI);
+	YAW_KI->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(YAW_KI);
+	yawBoxLayout->addRow(tr("KI:"), YAW_KI);
+	m_signalMapper->setMapping(YAW_KI, YAW_KI);
+	connect(YAW_KI, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *YAW_KD = new ParameterSpinBox(Parameters::YAW_KD);
+	YAW_KD->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(YAW_KD);
+	yawBoxLayout->addRow(tr("KD:"), YAW_KD);
+	m_signalMapper->setMapping(YAW_KD, YAW_KD);
+	connect(YAW_KD, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *YAW_ILIMIT = new ParameterSpinBox(Parameters::YAW_ILIMIT);
+	YAW_ILIMIT->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(YAW_ILIMIT);
+	yawBoxLayout->addRow(tr("ILIMIT:"), YAW_ILIMIT);
+	m_signalMapper->setMapping(YAW_ILIMIT, YAW_ILIMIT);
+	connect(YAW_ILIMIT, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *YAW_RESOLUTIONFILTER = new ParameterSpinBox(Parameters::YAW_RESOLUTIONFILTER);
+	YAW_RESOLUTIONFILTER->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(YAW_RESOLUTIONFILTER);
+	yawBoxLayout->addRow(tr("RESOLUTIONFILTER:"), YAW_RESOLUTIONFILTER);
+	m_signalMapper->setMapping(YAW_RESOLUTIONFILTER, YAW_RESOLUTIONFILTER);
+	connect(YAW_RESOLUTIONFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *YAW_AVERAGINGFILTER = new ParameterSpinBox(Parameters::YAW_AVERAGINGFILTER);
+	YAW_AVERAGINGFILTER->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(YAW_AVERAGINGFILTER);
+	yawBoxLayout->addRow(tr("AVERAGINGFILTER:"), YAW_AVERAGINGFILTER);
+	m_signalMapper->setMapping(YAW_AVERAGINGFILTER, YAW_AVERAGINGFILTER);
+	connect(YAW_AVERAGINGFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	QGroupBox *rollBox = new QGroupBox(tr("ROLL"));
+	QFormLayout *rollBoxLayout = new QFormLayout();
+	rollBox->setLayout(rollBoxLayout);
+
+	ParameterSpinBox *ROLL_KP = new ParameterSpinBox(Parameters::ROLL_KP);
+	ROLL_KP->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(ROLL_KP);
+	rollBoxLayout->addRow(tr("KP:"), ROLL_KP);
+	m_signalMapper->setMapping(ROLL_KP, ROLL_KP);
+	connect(ROLL_KP, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *ROLL_KI = new ParameterSpinBox(Parameters::ROLL_KI);
+	ROLL_KI->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(ROLL_KI);
+	rollBoxLayout->addRow(tr("KI:"), ROLL_KI);
+	m_signalMapper->setMapping(ROLL_KI, ROLL_KI);
+	connect(ROLL_KI, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *ROLL_KD = new ParameterSpinBox(Parameters::ROLL_KD);
+	ROLL_KD->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(ROLL_KD);
+	rollBoxLayout->addRow(tr("KD:"), ROLL_KD);
+	m_signalMapper->setMapping(ROLL_KD, ROLL_KD);
+	connect(ROLL_KD, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *ROLL_ILIMIT = new ParameterSpinBox(Parameters::ROLL_ILIMIT);
+	ROLL_ILIMIT->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(ROLL_ILIMIT);
+	rollBoxLayout->addRow(tr("ILIMIT:"), ROLL_ILIMIT);
+	m_signalMapper->setMapping(ROLL_ILIMIT, ROLL_ILIMIT);
+	connect(ROLL_ILIMIT, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *ROLL_RESOLUTIONFILTER = new ParameterSpinBox(Parameters::ROLL_RESOLUTIONFILTER);
+	ROLL_RESOLUTIONFILTER->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(ROLL_RESOLUTIONFILTER);
+	rollBoxLayout->addRow(tr("RESOLUTIONFILTER:"), ROLL_RESOLUTIONFILTER);
+	m_signalMapper->setMapping(ROLL_RESOLUTIONFILTER, ROLL_RESOLUTIONFILTER);
+	connect(ROLL_RESOLUTIONFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *ROLL_AVERAGINGFILTER = new ParameterSpinBox(Parameters::ROLL_AVERAGINGFILTER);
+	ROLL_AVERAGINGFILTER->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(ROLL_AVERAGINGFILTER);
+	rollBoxLayout->addRow(tr("AVERAGINGFILTER:"), ROLL_AVERAGINGFILTER);
+	m_signalMapper->setMapping(ROLL_AVERAGINGFILTER, ROLL_AVERAGINGFILTER);
+	connect(ROLL_AVERAGINGFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	QGroupBox *pitchBox = new QGroupBox(tr("PITCH"));
+	QFormLayout *pitchBoxLayout = new QFormLayout();
+	pitchBox->setLayout(pitchBoxLayout);
+
+	ParameterSpinBox *PITCH_KP = new ParameterSpinBox(Parameters::PITCH_KP);
+	PITCH_KP->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(PITCH_KP);
+	pitchBoxLayout->addRow(tr("KP:"), PITCH_KP);
+	m_signalMapper->setMapping(PITCH_KP, PITCH_KP);
+	connect(PITCH_KP, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *PITCH_KI = new ParameterSpinBox(Parameters::PITCH_KI);
+	PITCH_KI->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(PITCH_KI);
+	pitchBoxLayout->addRow(tr("KI:"), PITCH_KI);
+	m_signalMapper->setMapping(PITCH_KI, PITCH_KI);
+	connect(PITCH_KI, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *PITCH_KD = new ParameterSpinBox(Parameters::PITCH_KD);
+	PITCH_KD->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(PITCH_KD);
+	pitchBoxLayout->addRow(tr("KD:"), PITCH_KD);
+	m_signalMapper->setMapping(PITCH_KD, PITCH_KD);
+	connect(PITCH_KD, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *PITCH_ILIMIT = new ParameterSpinBox(Parameters::PITCH_ILIMIT);
+	PITCH_ILIMIT->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(PITCH_ILIMIT);
+	pitchBoxLayout->addRow(tr("ILIMIT:"), PITCH_ILIMIT);
+	m_signalMapper->setMapping(PITCH_ILIMIT, PITCH_ILIMIT);
+	connect(PITCH_ILIMIT, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *PITCH_RESOLUTIONFILTER = new ParameterSpinBox(Parameters::PITCH_RESOLUTIONFILTER);
+	PITCH_RESOLUTIONFILTER->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(PITCH_RESOLUTIONFILTER);
+	pitchBoxLayout->addRow(tr("RESOLUTIONFILTER:"), PITCH_RESOLUTIONFILTER);
+	m_signalMapper->setMapping(PITCH_RESOLUTIONFILTER, PITCH_RESOLUTIONFILTER);
+	connect(PITCH_RESOLUTIONFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
+	ParameterSpinBox *PITCH_AVERAGINGFILTER = new ParameterSpinBox(Parameters::PITCH_AVERAGINGFILTER);
+	PITCH_AVERAGINGFILTER->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(PITCH_AVERAGINGFILTER);
+	pitchBoxLayout->addRow(tr("AVERAGINGFILTER:"), PITCH_AVERAGINGFILTER);
+	m_signalMapper->setMapping(PITCH_AVERAGINGFILTER, PITCH_AVERAGINGFILTER);
+	connect(PITCH_AVERAGINGFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+
 	QGroupBox *miscBox = new QGroupBox(tr("MISC"));
 	QFormLayout *miscBoxLayout = new QFormLayout();
 	miscBox->setLayout(miscBoxLayout);
@@ -25,13 +163,6 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	miscBoxLayout->addRow(tr("ACC_HORIZ_KI:"), MISC_ACC_HORIZ_KI);
 	m_signalMapper->setMapping(MISC_ACC_HORIZ_KI, MISC_ACC_HORIZ_KI);
 	connect(MISC_ACC_HORIZ_KI, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *MISC_ACC_RANGE = new ParameterSpinBox(Parameters::MISC_ACC_RANGE);
-	MISC_ACC_RANGE->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(MISC_ACC_RANGE);
-	miscBoxLayout->addRow(tr("ACC_RANGE:"), MISC_ACC_RANGE);
-	m_signalMapper->setMapping(MISC_ACC_RANGE, MISC_ACC_RANGE);
-	connect(MISC_ACC_RANGE, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
 
 	ParameterSpinBox *MISC_ACC_VERT_KI = new ParameterSpinBox(Parameters::MISC_ACC_VERT_KI);
 	MISC_ACC_VERT_KI->setRange(INT_MIN, 2147483647);
@@ -54,13 +185,6 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	m_signalMapper->setMapping(MISC_IDLE_SPEED, MISC_IDLE_SPEED);
 	connect(MISC_IDLE_SPEED, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
 
-	ParameterSpinBox *MISC_SKIP_CONTROL_CYCLES = new ParameterSpinBox(Parameters::MISC_SKIP_CONTROL_CYCLES);
-	MISC_SKIP_CONTROL_CYCLES->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(MISC_SKIP_CONTROL_CYCLES);
-	miscBoxLayout->addRow(tr("SKIP_CONTROL_CYCLES:"), MISC_SKIP_CONTROL_CYCLES);
-	m_signalMapper->setMapping(MISC_SKIP_CONTROL_CYCLES, MISC_SKIP_CONTROL_CYCLES);
-	connect(MISC_SKIP_CONTROL_CYCLES, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
 	ParameterSpinBox *MISC_START_THRESHOLD = new ParameterSpinBox(Parameters::MISC_START_THRESHOLD);
 	MISC_START_THRESHOLD->setRange(INT_MIN, 2147483647);
 	m_parameterSpinBoxes.append(MISC_START_THRESHOLD);
@@ -75,97 +199,19 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	m_signalMapper->setMapping(MISC_STOP_THRESHOLD, MISC_STOP_THRESHOLD);
 	connect(MISC_STOP_THRESHOLD, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
 
-	QGroupBox *pitchBox = new QGroupBox(tr("PITCH"));
-	QFormLayout *pitchBoxLayout = new QFormLayout();
-	pitchBox->setLayout(pitchBoxLayout);
+	ParameterSpinBox *MISC_SKIP_CONTROL_CYCLES = new ParameterSpinBox(Parameters::MISC_SKIP_CONTROL_CYCLES);
+	MISC_SKIP_CONTROL_CYCLES->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(MISC_SKIP_CONTROL_CYCLES);
+	miscBoxLayout->addRow(tr("SKIP_CONTROL_CYCLES:"), MISC_SKIP_CONTROL_CYCLES);
+	m_signalMapper->setMapping(MISC_SKIP_CONTROL_CYCLES, MISC_SKIP_CONTROL_CYCLES);
+	connect(MISC_SKIP_CONTROL_CYCLES, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
 
-	ParameterSpinBox *PITCH_AVERAGINGFILTER = new ParameterSpinBox(Parameters::PITCH_AVERAGINGFILTER);
-	PITCH_AVERAGINGFILTER->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(PITCH_AVERAGINGFILTER);
-	pitchBoxLayout->addRow(tr("AVERAGINGFILTER:"), PITCH_AVERAGINGFILTER);
-	m_signalMapper->setMapping(PITCH_AVERAGINGFILTER, PITCH_AVERAGINGFILTER);
-	connect(PITCH_AVERAGINGFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *PITCH_ILIMIT = new ParameterSpinBox(Parameters::PITCH_ILIMIT);
-	PITCH_ILIMIT->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(PITCH_ILIMIT);
-	pitchBoxLayout->addRow(tr("ILIMIT:"), PITCH_ILIMIT);
-	m_signalMapper->setMapping(PITCH_ILIMIT, PITCH_ILIMIT);
-	connect(PITCH_ILIMIT, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *PITCH_KD = new ParameterSpinBox(Parameters::PITCH_KD);
-	PITCH_KD->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(PITCH_KD);
-	pitchBoxLayout->addRow(tr("KD:"), PITCH_KD);
-	m_signalMapper->setMapping(PITCH_KD, PITCH_KD);
-	connect(PITCH_KD, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *PITCH_KI = new ParameterSpinBox(Parameters::PITCH_KI);
-	PITCH_KI->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(PITCH_KI);
-	pitchBoxLayout->addRow(tr("KI:"), PITCH_KI);
-	m_signalMapper->setMapping(PITCH_KI, PITCH_KI);
-	connect(PITCH_KI, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *PITCH_KP = new ParameterSpinBox(Parameters::PITCH_KP);
-	PITCH_KP->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(PITCH_KP);
-	pitchBoxLayout->addRow(tr("KP:"), PITCH_KP);
-	m_signalMapper->setMapping(PITCH_KP, PITCH_KP);
-	connect(PITCH_KP, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *PITCH_RESOLUTIONFILTER = new ParameterSpinBox(Parameters::PITCH_RESOLUTIONFILTER);
-	PITCH_RESOLUTIONFILTER->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(PITCH_RESOLUTIONFILTER);
-	pitchBoxLayout->addRow(tr("RESOLUTIONFILTER:"), PITCH_RESOLUTIONFILTER);
-	m_signalMapper->setMapping(PITCH_RESOLUTIONFILTER, PITCH_RESOLUTIONFILTER);
-	connect(PITCH_RESOLUTIONFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	QGroupBox *rollBox = new QGroupBox(tr("ROLL"));
-	QFormLayout *rollBoxLayout = new QFormLayout();
-	rollBox->setLayout(rollBoxLayout);
-
-	ParameterSpinBox *ROLL_AVERAGINGFILTER = new ParameterSpinBox(Parameters::ROLL_AVERAGINGFILTER);
-	ROLL_AVERAGINGFILTER->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(ROLL_AVERAGINGFILTER);
-	rollBoxLayout->addRow(tr("AVERAGINGFILTER:"), ROLL_AVERAGINGFILTER);
-	m_signalMapper->setMapping(ROLL_AVERAGINGFILTER, ROLL_AVERAGINGFILTER);
-	connect(ROLL_AVERAGINGFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *ROLL_ILIMIT = new ParameterSpinBox(Parameters::ROLL_ILIMIT);
-	ROLL_ILIMIT->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(ROLL_ILIMIT);
-	rollBoxLayout->addRow(tr("ILIMIT:"), ROLL_ILIMIT);
-	m_signalMapper->setMapping(ROLL_ILIMIT, ROLL_ILIMIT);
-	connect(ROLL_ILIMIT, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *ROLL_KD = new ParameterSpinBox(Parameters::ROLL_KD);
-	ROLL_KD->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(ROLL_KD);
-	rollBoxLayout->addRow(tr("KD:"), ROLL_KD);
-	m_signalMapper->setMapping(ROLL_KD, ROLL_KD);
-	connect(ROLL_KD, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *ROLL_KI = new ParameterSpinBox(Parameters::ROLL_KI);
-	ROLL_KI->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(ROLL_KI);
-	rollBoxLayout->addRow(tr("KI:"), ROLL_KI);
-	m_signalMapper->setMapping(ROLL_KI, ROLL_KI);
-	connect(ROLL_KI, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *ROLL_KP = new ParameterSpinBox(Parameters::ROLL_KP);
-	ROLL_KP->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(ROLL_KP);
-	rollBoxLayout->addRow(tr("KP:"), ROLL_KP);
-	m_signalMapper->setMapping(ROLL_KP, ROLL_KP);
-	connect(ROLL_KP, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *ROLL_RESOLUTIONFILTER = new ParameterSpinBox(Parameters::ROLL_RESOLUTIONFILTER);
-	ROLL_RESOLUTIONFILTER->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(ROLL_RESOLUTIONFILTER);
-	rollBoxLayout->addRow(tr("RESOLUTIONFILTER:"), ROLL_RESOLUTIONFILTER);
-	m_signalMapper->setMapping(ROLL_RESOLUTIONFILTER, ROLL_RESOLUTIONFILTER);
-	connect(ROLL_RESOLUTIONFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
+	ParameterSpinBox *MISC_ACC_RANGE = new ParameterSpinBox(Parameters::MISC_ACC_RANGE);
+	MISC_ACC_RANGE->setRange(INT_MIN, 2147483647);
+	m_parameterSpinBoxes.append(MISC_ACC_RANGE);
+	miscBoxLayout->addRow(tr("ACC_RANGE:"), MISC_ACC_RANGE);
+	m_signalMapper->setMapping(MISC_ACC_RANGE, MISC_ACC_RANGE);
+	connect(MISC_ACC_RANGE, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
 
 	QGroupBox *specialBox = new QGroupBox(tr("SPECIAL"));
 	QFormLayout *specialBoxLayout = new QFormLayout();
@@ -178,57 +224,11 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	m_signalMapper->setMapping(SPECIAL_BATT_VOLTAGE, SPECIAL_BATT_VOLTAGE);
 	connect(SPECIAL_BATT_VOLTAGE, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
 
-	QGroupBox *yawBox = new QGroupBox(tr("YAW"));
-	QFormLayout *yawBoxLayout = new QFormLayout();
-	yawBox->setLayout(yawBoxLayout);
-
-	ParameterSpinBox *YAW_AVERAGINGFILTER = new ParameterSpinBox(Parameters::YAW_AVERAGINGFILTER);
-	YAW_AVERAGINGFILTER->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(YAW_AVERAGINGFILTER);
-	yawBoxLayout->addRow(tr("AVERAGINGFILTER:"), YAW_AVERAGINGFILTER);
-	m_signalMapper->setMapping(YAW_AVERAGINGFILTER, YAW_AVERAGINGFILTER);
-	connect(YAW_AVERAGINGFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *YAW_ILIMIT = new ParameterSpinBox(Parameters::YAW_ILIMIT);
-	YAW_ILIMIT->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(YAW_ILIMIT);
-	yawBoxLayout->addRow(tr("ILIMIT:"), YAW_ILIMIT);
-	m_signalMapper->setMapping(YAW_ILIMIT, YAW_ILIMIT);
-	connect(YAW_ILIMIT, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *YAW_KD = new ParameterSpinBox(Parameters::YAW_KD);
-	YAW_KD->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(YAW_KD);
-	yawBoxLayout->addRow(tr("KD:"), YAW_KD);
-	m_signalMapper->setMapping(YAW_KD, YAW_KD);
-	connect(YAW_KD, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *YAW_KI = new ParameterSpinBox(Parameters::YAW_KI);
-	YAW_KI->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(YAW_KI);
-	yawBoxLayout->addRow(tr("KI:"), YAW_KI);
-	m_signalMapper->setMapping(YAW_KI, YAW_KI);
-	connect(YAW_KI, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *YAW_KP = new ParameterSpinBox(Parameters::YAW_KP);
-	YAW_KP->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(YAW_KP);
-	yawBoxLayout->addRow(tr("KP:"), YAW_KP);
-	m_signalMapper->setMapping(YAW_KP, YAW_KP);
-	connect(YAW_KP, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	ParameterSpinBox *YAW_RESOLUTIONFILTER = new ParameterSpinBox(Parameters::YAW_RESOLUTIONFILTER);
-	YAW_RESOLUTIONFILTER->setRange(INT_MIN, 2147483647);
-	m_parameterSpinBoxes.append(YAW_RESOLUTIONFILTER);
-	yawBoxLayout->addRow(tr("RESOLUTIONFILTER:"), YAW_RESOLUTIONFILTER);
-	m_signalMapper->setMapping(YAW_RESOLUTIONFILTER, YAW_RESOLUTIONFILTER);
-	connect(YAW_RESOLUTIONFILTER, SIGNAL(valueChanged(int)), m_signalMapper, SLOT(map()));
-
-	parameterGroupsLayout->addWidget(miscBox);
-	parameterGroupsLayout->addWidget(pitchBox);
-	parameterGroupsLayout->addWidget(rollBox);
-	parameterGroupsLayout->addWidget(specialBox);
 	parameterGroupsLayout->addWidget(yawBox);
+	parameterGroupsLayout->addWidget(rollBox);
+	parameterGroupsLayout->addWidget(pitchBox);
+	parameterGroupsLayout->addWidget(miscBox);
+	parameterGroupsLayout->addWidget(specialBox);
 
 	// Buttons
 
