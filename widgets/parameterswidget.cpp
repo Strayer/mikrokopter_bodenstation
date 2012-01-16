9e38a84394ac343f3888b710be8f6a3d710693ba
Sven Grunewaldt <strayer@olle-orks.org>
2012-01-16 21:55:21 +0100

    Die wunderbare Welt der Makros

--- widgets/parameterswidget.cpp
+++ widgets/parameterswidget.cpp
@@ -6,6 +6,14 @@
 #include <QtGui>
 #include <QSharedPointer>
 
+#define create_parameter_widget(param_id, layout) { \
+	ParameterControlWidget *param_widget = new ParameterControlWidget(param_id, this); \
+	m_parameterControlWidgets.append(param_widget); \
+	layout->addWidget(param_widget); \
+	m_signalMapper->setMapping(param_widget, param_widget); \
+	connect(param_widget, SIGNAL(editingFinished()), m_signalMapper, SLOT(map())); \
+}
+
 ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	QWidget(parent), m_mainWindow(mainWindow)
 {
@@ -19,41 +27,12 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	QVBoxLayout *yawLayout = new QVBoxLayout();
 	yawBox->setLayout(yawLayout);
 
-	ParameterControlWidget *YAW_KP = new ParameterControlWidget(Parameters::YAW_KP, this);
-	m_parameterControlWidgets.append(YAW_KP);
-	yawLayout->addWidget(YAW_KP);
-	m_signalMapper->setMapping(YAW_KP, YAW_KP);
-	connect(YAW_KP, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *YAW_KI = new ParameterControlWidget(Parameters::YAW_KI, this);
-	m_parameterControlWidgets.append(YAW_KI);
-	yawLayout->addWidget(YAW_KI);
-	m_signalMapper->setMapping(YAW_KI, YAW_KI);
-	connect(YAW_KI, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *YAW_KD = new ParameterControlWidget(Parameters::YAW_KD, this);
-	m_parameterControlWidgets.append(YAW_KD);
-	yawLayout->addWidget(YAW_KD);
-	m_signalMapper->setMapping(YAW_KD, YAW_KD);
-	connect(YAW_KD, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *YAW_ILIMIT = new ParameterControlWidget(Parameters::YAW_ILIMIT, this);
-	m_parameterControlWidgets.append(YAW_ILIMIT);
-	yawLayout->addWidget(YAW_ILIMIT);
-	m_signalMapper->setMapping(YAW_ILIMIT, YAW_ILIMIT);
-	connect(YAW_ILIMIT, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *YAW_RESOLUTIONFILTER = new ParameterControlWidget(Parameters::YAW_RESOLUTIONFILTER, this);
-	m_parameterControlWidgets.append(YAW_RESOLUTIONFILTER);
-	yawLayout->addWidget(YAW_RESOLUTIONFILTER);
-	m_signalMapper->setMapping(YAW_RESOLUTIONFILTER, YAW_RESOLUTIONFILTER);
-	connect(YAW_RESOLUTIONFILTER, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *YAW_AVERAGINGFILTER = new ParameterControlWidget(Parameters::YAW_AVERAGINGFILTER, this);
-	m_parameterControlWidgets.append(YAW_AVERAGINGFILTER);
-	yawLayout->addWidget(YAW_AVERAGINGFILTER);
-	m_signalMapper->setMapping(YAW_AVERAGINGFILTER, YAW_AVERAGINGFILTER);
-	connect(YAW_AVERAGINGFILTER, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
+	create_parameter_widget(Parameters::YAW_KP, yawLayout);
+	create_parameter_widget(Parameters::YAW_KI, yawLayout);
+	create_parameter_widget(Parameters::YAW_KD, yawLayout);
+	create_parameter_widget(Parameters::YAW_ILIMIT, yawLayout);
+	create_parameter_widget(Parameters::YAW_RESOLUTIONFILTER, yawLayout);
+	create_parameter_widget(Parameters::YAW_AVERAGINGFILTER, yawLayout);
 
 	yawLayout->addStretch();
 
@@ -61,41 +40,12 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	QVBoxLayout *rollLayout = new QVBoxLayout();
 	rollBox->setLayout(rollLayout);
 
-	ParameterControlWidget *ROLL_KP = new ParameterControlWidget(Parameters::ROLL_KP, this);
-	m_parameterControlWidgets.append(ROLL_KP);
-	rollLayout->addWidget(ROLL_KP);
-	m_signalMapper->setMapping(ROLL_KP, ROLL_KP);
-	connect(ROLL_KP, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *ROLL_KI = new ParameterControlWidget(Parameters::ROLL_KI, this);
-	m_parameterControlWidgets.append(ROLL_KI);
-	rollLayout->addWidget(ROLL_KI);
-	m_signalMapper->setMapping(ROLL_KI, ROLL_KI);
-	connect(ROLL_KI, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *ROLL_KD = new ParameterControlWidget(Parameters::ROLL_KD, this);
-	m_parameterControlWidgets.append(ROLL_KD);
-	rollLayout->addWidget(ROLL_KD);
-	m_signalMapper->setMapping(ROLL_KD, ROLL_KD);
-	connect(ROLL_KD, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *ROLL_ILIMIT = new ParameterControlWidget(Parameters::ROLL_ILIMIT, this);
-	m_parameterControlWidgets.append(ROLL_ILIMIT);
-	rollLayout->addWidget(ROLL_ILIMIT);
-	m_signalMapper->setMapping(ROLL_ILIMIT, ROLL_ILIMIT);
-	connect(ROLL_ILIMIT, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *ROLL_RESOLUTIONFILTER = new ParameterControlWidget(Parameters::ROLL_RESOLUTIONFILTER, this);
-	m_parameterControlWidgets.append(ROLL_RESOLUTIONFILTER);
-	rollLayout->addWidget(ROLL_RESOLUTIONFILTER);
-	m_signalMapper->setMapping(ROLL_RESOLUTIONFILTER, ROLL_RESOLUTIONFILTER);
-	connect(ROLL_RESOLUTIONFILTER, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *ROLL_AVERAGINGFILTER = new ParameterControlWidget(Parameters::ROLL_AVERAGINGFILTER, this);
-	m_parameterControlWidgets.append(ROLL_AVERAGINGFILTER);
-	rollLayout->addWidget(ROLL_AVERAGINGFILTER);
-	m_signalMapper->setMapping(ROLL_AVERAGINGFILTER, ROLL_AVERAGINGFILTER);
-	connect(ROLL_AVERAGINGFILTER, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
+	create_parameter_widget(Parameters::ROLL_KP, rollLayout);
+	create_parameter_widget(Parameters::ROLL_KI, rollLayout);
+	create_parameter_widget(Parameters::ROLL_KD, rollLayout);
+	create_parameter_widget(Parameters::ROLL_ILIMIT, rollLayout);
+	create_parameter_widget(Parameters::ROLL_RESOLUTIONFILTER, rollLayout);
+	create_parameter_widget(Parameters::ROLL_AVERAGINGFILTER, rollLayout);
 
 	rollLayout->addStretch();
 
@@ -103,41 +53,12 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	QVBoxLayout *pitchLayout = new QVBoxLayout();
 	pitchBox->setLayout(pitchLayout);
 
-	ParameterControlWidget *PITCH_KP = new ParameterControlWidget(Parameters::PITCH_KP, this);
-	m_parameterControlWidgets.append(PITCH_KP);
-	pitchLayout->addWidget(PITCH_KP);
-	m_signalMapper->setMapping(PITCH_KP, PITCH_KP);
-	connect(PITCH_KP, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *PITCH_KI = new ParameterControlWidget(Parameters::PITCH_KI, this);
-	m_parameterControlWidgets.append(PITCH_KI);
-	pitchLayout->addWidget(PITCH_KI);
-	m_signalMapper->setMapping(PITCH_KI, PITCH_KI);
-	connect(PITCH_KI, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *PITCH_KD = new ParameterControlWidget(Parameters::PITCH_KD, this);
-	m_parameterControlWidgets.append(PITCH_KD);
-	pitchLayout->addWidget(PITCH_KD);
-	m_signalMapper->setMapping(PITCH_KD, PITCH_KD);
-	connect(PITCH_KD, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *PITCH_ILIMIT = new ParameterControlWidget(Parameters::PITCH_ILIMIT, this);
-	m_parameterControlWidgets.append(PITCH_ILIMIT);
-	pitchLayout->addWidget(PITCH_ILIMIT);
-	m_signalMapper->setMapping(PITCH_ILIMIT, PITCH_ILIMIT);
-	connect(PITCH_ILIMIT, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *PITCH_RESOLUTIONFILTER = new ParameterControlWidget(Parameters::PITCH_RESOLUTIONFILTER, this);
-	m_parameterControlWidgets.append(PITCH_RESOLUTIONFILTER);
-	pitchLayout->addWidget(PITCH_RESOLUTIONFILTER);
-	m_signalMapper->setMapping(PITCH_RESOLUTIONFILTER, PITCH_RESOLUTIONFILTER);
-	connect(PITCH_RESOLUTIONFILTER, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *PITCH_AVERAGINGFILTER = new ParameterControlWidget(Parameters::PITCH_AVERAGINGFILTER, this);
-	m_parameterControlWidgets.append(PITCH_AVERAGINGFILTER);
-	pitchLayout->addWidget(PITCH_AVERAGINGFILTER);
-	m_signalMapper->setMapping(PITCH_AVERAGINGFILTER, PITCH_AVERAGINGFILTER);
-	connect(PITCH_AVERAGINGFILTER, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
+	create_parameter_widget(Parameters::PITCH_KP, pitchLayout);
+	create_parameter_widget(Parameters::PITCH_KI, pitchLayout);
+	create_parameter_widget(Parameters::PITCH_KD, pitchLayout);
+	create_parameter_widget(Parameters::PITCH_ILIMIT, pitchLayout);
+	create_parameter_widget(Parameters::PITCH_RESOLUTIONFILTER, pitchLayout);
+	create_parameter_widget(Parameters::PITCH_AVERAGINGFILTER, pitchLayout);
 
 	pitchLayout->addStretch();
 
@@ -145,53 +66,14 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	QVBoxLayout *miscLayout = new QVBoxLayout();
 	miscBox->setLayout(miscLayout);
 
-	ParameterControlWidget *MISC_ACC_HORIZ_KI = new ParameterControlWidget(Parameters::MISC_ACC_HORIZ_KI, this);
-	m_parameterControlWidgets.append(MISC_ACC_HORIZ_KI);
-	miscLayout->addWidget(MISC_ACC_HORIZ_KI);
-	m_signalMapper->setMapping(MISC_ACC_HORIZ_KI, MISC_ACC_HORIZ_KI);
-	connect(MISC_ACC_HORIZ_KI, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *MISC_ACC_VERT_KI = new ParameterControlWidget(Parameters::MISC_ACC_VERT_KI, this);
-	m_parameterControlWidgets.append(MISC_ACC_VERT_KI);
-	miscLayout->addWidget(MISC_ACC_VERT_KI);
-	m_signalMapper->setMapping(MISC_ACC_VERT_KI, MISC_ACC_VERT_KI);
-	connect(MISC_ACC_VERT_KI, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *MISC_COMPASS_KI = new ParameterControlWidget(Parameters::MISC_COMPASS_KI, this);
-	m_parameterControlWidgets.append(MISC_COMPASS_KI);
-	miscLayout->addWidget(MISC_COMPASS_KI);
-	m_signalMapper->setMapping(MISC_COMPASS_KI, MISC_COMPASS_KI);
-	connect(MISC_COMPASS_KI, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *MISC_IDLE_SPEED = new ParameterControlWidget(Parameters::MISC_IDLE_SPEED, this);
-	m_parameterControlWidgets.append(MISC_IDLE_SPEED);
-	miscLayout->addWidget(MISC_IDLE_SPEED);
-	m_signalMapper->setMapping(MISC_IDLE_SPEED, MISC_IDLE_SPEED);
-	connect(MISC_IDLE_SPEED, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *MISC_START_THRESHOLD = new ParameterControlWidget(Parameters::MISC_START_THRESHOLD, this);
-	m_parameterControlWidgets.append(MISC_START_THRESHOLD);
-	miscLayout->addWidget(MISC_START_THRESHOLD);
-	m_signalMapper->setMapping(MISC_START_THRESHOLD, MISC_START_THRESHOLD);
-	connect(MISC_START_THRESHOLD, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *MISC_STOP_THRESHOLD = new ParameterControlWidget(Parameters::MISC_STOP_THRESHOLD, this);
-	m_parameterControlWidgets.append(MISC_STOP_THRESHOLD);
-	miscLayout->addWidget(MISC_STOP_THRESHOLD);
-	m_signalMapper->setMapping(MISC_STOP_THRESHOLD, MISC_STOP_THRESHOLD);
-	connect(MISC_STOP_THRESHOLD, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *MISC_SKIP_CONTROL_CYCLES = new ParameterControlWidget(Parameters::MISC_SKIP_CONTROL_CYCLES, this);
-	m_parameterControlWidgets.append(MISC_SKIP_CONTROL_CYCLES);
-	miscLayout->addWidget(MISC_SKIP_CONTROL_CYCLES);
-	m_signalMapper->setMapping(MISC_SKIP_CONTROL_CYCLES, MISC_SKIP_CONTROL_CYCLES);
-	connect(MISC_SKIP_CONTROL_CYCLES, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
-
-	ParameterControlWidget *MISC_ACC_RANGE = new ParameterControlWidget(Parameters::MISC_ACC_RANGE, this);
-	m_parameterControlWidgets.append(MISC_ACC_RANGE);
-	miscLayout->addWidget(MISC_ACC_RANGE);
-	m_signalMapper->setMapping(MISC_ACC_RANGE, MISC_ACC_RANGE);
-	connect(MISC_ACC_RANGE, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
+	create_parameter_widget(Parameters::MISC_ACC_HORIZ_KI, miscLayout);
+	create_parameter_widget(Parameters::MISC_ACC_VERT_KI, miscLayout);
+	create_parameter_widget(Parameters::MISC_COMPASS_KI, miscLayout);
+	create_parameter_widget(Parameters::MISC_IDLE_SPEED, miscLayout);
+	create_parameter_widget(Parameters::MISC_START_THRESHOLD, miscLayout);
+	create_parameter_widget(Parameters::MISC_STOP_THRESHOLD, miscLayout);
+	create_parameter_widget(Parameters::MISC_SKIP_CONTROL_CYCLES, miscLayout);
+	create_parameter_widget(Parameters::MISC_ACC_RANGE, miscLayout);
 
 	miscLayout->addStretch();
 
@@ -199,11 +81,7 @@ ParametersWidget::ParametersWidget(MainWindow *mainWindow, QWidget *parent) :
 	QVBoxLayout *specialLayout = new QVBoxLayout();
 	specialBox->setLayout(specialLayout);
 
-	ParameterControlWidget *SPECIAL_BATT_VOLTAGE = new ParameterControlWidget(Parameters::SPECIAL_BATT_VOLTAGE, this);
-	m_parameterControlWidgets.append(SPECIAL_BATT_VOLTAGE);
-	specialLayout->addWidget(SPECIAL_BATT_VOLTAGE);
-	m_signalMapper->setMapping(SPECIAL_BATT_VOLTAGE, SPECIAL_BATT_VOLTAGE);
-	connect(SPECIAL_BATT_VOLTAGE, SIGNAL(editingFinished()), m_signalMapper, SLOT(map()));
+	create_parameter_widget(Parameters::SPECIAL_BATT_VOLTAGE, specialLayout);
 
 	specialLayout->addStretch();
 
